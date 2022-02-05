c3c8d9ccc71a3ab1b64652b660cccd9ce8571e68
Laise <laise94@hotmail.com>
2022-02-04 21:52:23 -0300

    requisito 1 e 8 , modifiquei arquivo helpers,chamei a funçao no script js e implementei os tests

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,7 +1,14 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async (value) => {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${value}`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
 };
-
+fetchProducts();
 if (typeof module !== 'undefined') {
   module.exports = {
     fetchProducts,
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -1,8 +1,31 @@
-require('../mocks/fetchSimulator');
-const { fetchProducts } = require('../helpers/fetchProducts');
-const computadorSearch = require('../mocks/search');
-
-describe('1 - Teste a função fecthProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+require("../mocks/fetchSimulator");
+const { fetchProducts } = require("../helpers/fetchProducts");
+const computadorSearch = require("../mocks/search");
+//referencia https://jestjs.io/pt-BR/docs/asynchronous
+describe("1 - Teste a função fecthProducts", () => {
+  test("se fetchProducts é uma função", async () => {
+    expect(typeof fetchProducts).toBe('function');
+  });
+  //Execute a função fetchProducts com o argumento "computador" e teste se fetch foi chamada;
+  test("se fetch foi chamada", async () => {
+    await fetchProducts("computador");
+    expect(fetch).toHaveBeenCalled();
+  });
+  //Teste se, ao chamar a função fetchProducts com o argumento "computador", a função fetch utiliza o endpoint "https://api.mercadolibre.com/sites/MLB/search?q=computador";
+  test('se, ao chamar a função fetchProducts com o argumento computador", a função fetch utiliza o endpoint "https://api.mercadolibre.com/sites/MLB/search?q=computador', async () => {
+    await fetchProducts("computador");
+    expect(fetch).toHaveBeenCalledWith(
+      "https://api.mercadolibre.com/sites/MLB/search?q=computador"
+    );
+  });
+  //Teste se o retorno da função fetchProducts com o argumento "computador" é uma estrutura de dados igual ao objeto computadorSearch, que já está importado no arquivo.
+  test('se o retorno da função fetchProducts com o argumento "computador" é uma estrutura de dados igual ao objeto computadorSearch', async () => {
+    const resp = await fetchProducts('computador');
+    expect(resp).toEqual(computadorSearch);
+  });
+  //Teste se, ao chamar a função fetchProducts sem argumento, retorna um erro com a mensagem: You must provide an url. Dica: Lembre-se de usar o new Error('mensagem esperada aqui') para comparar com o objeto retornado da API.
+  test('se, a função fetchProducts sem argumetos retorna um erro com a mensagem: You must provide an url', async () => {
+    const semparam = await fetchProducts();
+    expect(semparam).toEqual(new Error('You must provide an url'));
+  });
 });
