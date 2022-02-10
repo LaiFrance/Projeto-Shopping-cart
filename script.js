b4ad197aa50dccc3b2075228048f5dd87ff53585
Laise <laise94@hotmail.com>
2022-02-10 00:44:21 -0300

    adicionando estilo matrix ao css

--- script.js
+++ script.js
@@ -101,7 +101,7 @@ function removecarregando() {
   setTimeout(function () {
     const h1 = document.querySelector('h1');
     h1.remove();
-    }, 2000);
+  }, 3000);
 }
 
 window.onload = () => {
